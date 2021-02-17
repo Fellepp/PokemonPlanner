187841b5b85a98770633152cdca09b8f68e75a20
Filip <johansenfilip@gmail.com>
2021-02-17 21:41:26 +0000

    typescraper add all types

--- src/webScraperDB.py
+++ src/webScraperDB.py
@@ -5,30 +5,48 @@ import pathlib
 import re
 
 galar_dex_url = "https://serebii.net/swordshield/galarpokedex.shtml"
+type_url = "https://pokemondb.net/type/{}"
+all_types = [
+    "fairy", "steel", "dark",
+    "dragon", "ghost", "rock",
+    "bug", "psychic", "flying",
+    "ground", "poison", "fight",
+    "ice", "grass", "electric",
+    "water", "fire", "normal"
+]
 
 
-def getTypes(pokemon_data):
-    types = []
-    column = pokemon_data[4].find_all('a')
-    for index, type in enumerate(column):
-        types.append(re.search("type\/(.*?)\.gif", str(column[index].contents[0])).group(1))
-    return types
+def typeScraper(url):
+    type_dex = {'types': []}
+    for poketype in all_types:
+        temp_url = url.format(poketype)
+        print("Retrieving data for ", poketype, " types from url: ", temp_url, "\n")
+        # temp_url = url.format("fairy")
+        data = requests.get(temp_url)
+        soup = bs4.BeautifulSoup(data.text, 'html.parser')
+        typeSoup = soup.find_all('p', class_='type-fx-list')
+
+        type_dex['types'].append(typeParse(typeSoup, poketype))
+
+    return type_dex
+
+    # print(typeSoup[0].contents[1].get('href').split('/')[-1])
+    # print(typeSoup[2].contents[1].get('href').split('/')[-1])
+
+    # 0-1,3,5 & 1-1,3,5
 
 
-    return ""
+def typeParse(type_data, poketype):
+    typeparser = {}
+    print(poketype)
 
-def pokeParse(pokemon_data):
-    pokeparser = {}
+    typeparser['type'] = poketype
+    # typeparser['attack_se'] =
 
-    print("")
-
-    pokeparser['galar_no'] = pokemon_data[0].contents[0].strip()[1:]
-    pokeparser['name'] = pokemon_data[2].contents[1].contents[0].strip()
-    pokeparser['type'] = getTypes(pokemon_data)
-    return pokeparser
+    return typeparser
 
 
-def pokemonScrapper(url):
+def pokemonScraper(url):
     data = requests.get(url)
     soup = bs4.BeautifulSoup(data.text, 'html.parser')
     pokeSoup = soup.find_all('td', class_='fooinfo')
@@ -42,6 +60,23 @@ def pokemonScrapper(url):
     return galar_dex
 
 
+def pokeParse(pokemon_data):
+    pokeparser = {}
+
+    pokeparser['galar_no'] = pokemon_data[0].contents[0].strip()[1:]
+    pokeparser['name'] = pokemon_data[2].contents[1].contents[0].strip()
+    pokeparser['type'] = getTypes(pokemon_data)
+    return pokeparser
+
+
+def getTypes(pokemon_data):
+    types = []
+    column = pokemon_data[4].find_all('a')
+    for index, type in enumerate(column):
+        types.append(re.search("type\/(.*?)\.gif", str(column[index].contents[0])).group(1))
+    return types
+
+
 def saveToFile(galar_dex: dict):
     pathlib.Path("./pokemon_db").mkdir(exist_ok=True)
 
@@ -49,5 +84,13 @@ def saveToFile(galar_dex: dict):
         json.dump(galar_dex, pokemon_json, indent=4, sort_keys=True)
 
 
-#print(pokemonScrapper(galar_dex_url))
-saveToFile(pokemonScrapper(galar_dex_url))
+def saveToFileTypes(type_dex: dict):
+    pathlib.Path("./pokemon_db").mkdir(exist_ok=True)
+
+    with open('./pokemon_db/type_dex.json', 'w') as type_json:
+        json.dump(type_dex, type_json, indent=4, sort_keys=True)
+
+
+# print(pokemonScraper(galar_dex_url))
+# saveToFile(pokemonScraper(galar_dex_url))
+saveToFileTypes(typeScraper(type_url))
